ebdfd748ec4c88d24727edb1cd79ea4fd4476c34
Junchen Li <junchen@rushdigital.co.nz>
2021-11-10 14:49:23 +1300

    Updated sql script

--- ExpenseManagerTransactions/ExpenseManagerTransactionsDB/Transactions.sql
+++ ExpenseManagerTransactions/ExpenseManagerTransactionsDB/Transactions.sql
@@ -1,4 +1,17 @@
 CREATE TABLE [dbo].[Transactions]
-(
-  [Id] INT NOT NULL PRIMARY KEY
-)
+   (
+      [ID] INT  IDENTITY (1, 1) NOT NULL,
+      [Description]   NVARCHAR (50)  NOT NULL,
+      [CreatedAt]     NVARCHAR (50)  NOT NULL,
+      [UpdatedAt]     NVARCHAR (50),
+      [Type]          INT  NOT NULL,
+      [Value]         DECIMAL(5,2)   NOT NULL, 
+      PRIMARY KEY CLUSTERED ([ID] ASC), 
+      [UserID]        INT 
+      FOREIGN KEY     REFERENCES dbo.Users (ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+   )
+;
+
+GO
--- ExpenseManagerUsers/ExpenseManagerUsersDB/Users.sql
+++ ExpenseManagerUsers/ExpenseManagerUsersDB/Users.sql
@@ -1,4 +1,8 @@
-CREATE TABLE [dbo].[Users]
-(
-  [Id] INT NOT NULL PRIMARY KEY
-)
+CREATE TABLE [dbo].[Users] (
+    [ID]           INT  IDENTITY (1, 1) NOT NULL,
+    [UserName]     NVARCHAR (50)  NOT NULL,
+    [PasswordHash] NVARCHAR (MAX) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+GO
